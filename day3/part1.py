7ca5b98c0ad4f9f82ec2fbf8dbd7514170e1ea45
Adrian McGuinnes <adrian.m.mcg@gmail.com>
2022-12-04 10:30:22 +0000

    AOC 2022 D3 P1

---
+++ day3/part1.py
@@ -0,0 +1,18 @@
+import string  
+
+with  open('./day3/input.txt', 'r') as file:
+    data = file.read().split('\n')
+
+letters = string.ascii_lowercase + string.ascii_uppercase
+output = 0
+
+for rucksack in data:
+
+    half = len(rucksack)//2
+    c1, c2 = set(rucksack[:half]), set(rucksack[half:])
+    for priortiy, char in enumerate(letters):
+        if char in c1 and char in c2:
+            output += priortiy + 1
+print(output)
+# 14131 too high
+# 7463 to low
