fb80efc2685d72bf6ee4f351cb4b04a681c5f15c
Adrian McGuinnes <adrian.m.mcg@gmail.com>
2022-12-04 17:01:21 +0000

    AOC 2022 D4 P2

---
+++ day4/part2.py
@@ -0,0 +1,17 @@
+with  open('./day4/input.txt', 'r') as file:
+    data = file.read().split('\n')
+
+output = 0
+for pair in data:
+    p1, p2 = pair.split(',')
+    p1_start, p1_end = p1.split('-')
+    p2_start, p2_end = p2.split('-')
+    if (int(p1_start) <= int(p2_start) <= int(p1_end)):
+        output += 1
+    elif (int(p1_start) <= int(p2_end) <= int(p1_end)):
+        output += 1
+    elif (int(p2_start) <= int(p1_start) <= int(p2_end)):
+        output += 1
+    elif (int(p2_start) <= int(p1_end) <= int(p2_end)):
+        output += 1
+print(output)
